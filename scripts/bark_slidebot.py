bc1cc8afe1429e835f6e1414338bce18a23be2ee
Sebestyen <tirczkas@gmail.com>
2022-02-22 20:02:41 +0100

    Progress

--- scripts/bark_slidebot.py
+++ scripts/bark_slidebot.py
@@ -47,7 +47,7 @@ import rospy
 import moveit_commander
 import rospkg
 import actionlib
-from bark_msgs.msg import BoundingBoxDetectionAction, BoundingBoxDetectionGoal
+from bark_msgs.msg import BoundingBoxDetectionAction, BoundingBoxDetectionGoal, CameraProjectionsAction, CameraProjectionsGoal
 from cv_bridge import CvBridge
 import moveit_msgs.msg
 import geometry_msgs.msg
@@ -55,6 +55,7 @@ from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 from sensor_msgs.msg import JointState
 
+import tf
 import sys
 import copy
 import os
@@ -92,14 +93,18 @@ class BarkSlidebot:
 
         self.configs = rospy.get_param("/bark_slidebot_config")
         self.bbox_client = actionlib.SimpleActionClient('bounding_box_detection', BoundingBoxDetectionAction)
+        self.camera_projections_client = actionlib.SimpleActionClient('camera_projections', CameraProjectionsAction)
         self.bbox_client.wait_for_server()
+        self.camera_projections_client.wait_for_server()
         self.bridge = CvBridge()
+
+        self.tf_listener = tf.TransformListener()
     
     def print_configs(self):
         rospy.loginfo(self.configs)
 
-    def move_to_photo_pose(self):
-        photo_pose = self.configs["photo_jpose"]
+    def move_to_joint_pose(self, pose_name):
+        photo_pose = self.configs[pose_name]
         rospy.loginfo("Moving to photo pose...")
 
         joint_goal = JointState()
@@ -113,7 +118,6 @@ class BarkSlidebot:
                 self.group.execute(plan_trajectory, wait = True)
 
     def bbox_prediction(self, img):
-        # TODO: add python packages to Dockerfile
         goal = BoundingBoxDetectionGoal()
         goal.image = self.bridge.cv2_to_imgmsg(img, encoding='bgr8')
         # Fill in the goal here
@@ -121,19 +125,134 @@ class BarkSlidebot:
         self.bbox_client.wait_for_result(rospy.Duration.from_sec(5.0))
         return self.bbox_client.get_result()
 
-    def deproject_2d_point_to_3d(self):
-        # TODO
-        pass
+    def deproject_2d_points_to_3d(self, points):
+        goal = CameraProjectionsGoal()
+        goal.space_type = goal.IMAGE_2D
+        goal.header.frame_id = "base"
 
-    def move_to_pose_with_camera(self, pose):
-        # TODO
-        pass
+        for point in points:
+            p = geometry_msgs.msg.Point()
+            p.x = point[0]
+            p.y = point[1]
+            p.z = point[2]
+            goal.points.append(p)
+        
+        self.camera_projections_client.send_goal(goal)
+        self.camera_projections_client.wait_for_result(rospy.Duration.from_sec(5.0))
+
+        return self.camera_projections_client.get_result()
+
+
+    def move_to_pose_with_frame(self, pose):
+        '''
+        pose (geometry_msgs/PoseStamped)
+        '''
+        # We can get the name of the reference frame for this robot:
+        print("planning_frame: ", self.group.get_planning_frame())
+        transformer = tf.Transformer(True, rospy.Duration(10.0))
+        transform = geometry_msgs.msg.TransformStamped()
+        transform.header.frame_id = "world"
+        transform.child_frame_id = "base_link"
+        transform.transform.rotation.w = 1
+        transformer.setTransform(transform)
+
+        transform = geometry_msgs.msg.TransformStamped()
+        transform.header.frame_id = "base_link"
+        transform.child_frame_id = "base"
+        transform.transform.rotation.z = 1
+        transformer.setTransform(transform)
+
+        transform = geometry_msgs.msg.TransformStamped()
+        transform.header.frame_id = "base"
+        transform.child_frame_id = "target"
+        transform.transform.translation.x = pose.pose.position.x
+        transform.transform.translation.y = pose.pose.position.y
+        transform.transform.translation.z = pose.pose.position.z
+        transform.transform.rotation = pose.pose.orientation
+        transformer.setTransform(transform)
+
+        transform = geometry_msgs.msg.TransformStamped()
+        transform.header.frame_id = "target"
+        eef_link = self.group.get_end_effector_link()
+        transform.child_frame_id = eef_link
+        trans, rot = self.lookup_transform(eef_link, pose.header.frame_id)
+        transform.transform.translation = geometry_msgs.msg.Vector3(*trans)
+        transform.transform.rotation = geometry_msgs.msg.Quaternion(*rot)
+        print("transform: ", transform)
+        transformer.setTransform(transform)
+
+        trans, rot = transformer.lookupTransform("base_link", eef_link, rospy.Time(0))
+        pose_goal = geometry_msgs.msg.Pose()
+        pose_goal.position = geometry_msgs.msg.Point(*trans)
+        pose_goal.orientation = geometry_msgs.msg.Quaternion(*rot)
+
+        success, plan_trajectory, planning_time, error_code = self.group.plan(pose_goal)
+        if success:
+                self.group.execute(plan_trajectory, wait = True)
+
+    def lookup_transform(self, parent, child):
+        if self.tf_listener.frameExists(parent) and self.tf_listener.frameExists(child):
+            t = self.tf_listener.getLatestCommonTime(child, parent)
+            (trans, rot) = self.tf_listener.lookupTransform(child, parent, t)
+            return (trans, rot)
+        else:
+            return None
+
+    def enable_joint_constraints(self):
+        constraints = moveit_msgs.msg.Constraints()
+        joint_constraint = moveit_msgs.msg.JointConstraint()
+
+        constraints.name = "limit_joint_movement"
+
+        for joint_name, joint_pose in zip(self.group.get_active_joints(), self.group.get_current_joint_values()):
+            joint_constraint.position = joint_pose
+
+            joint_constraint.tolerance_above = pi/4
+            joint_constraint.tolerance_below = pi/4
+
+            joint_constraint.weight = 1
+
+            joint_constraint.joint_name = joint_name
+            constraints.joint_constraints.append(joint_constraint)
+
+        self.group.set_path_constraints(constraints)
+
+    def disable_joint_constraints(self):
+        self.group.set_path_constraints(None)
 
 if __name__=="__main__":
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('bark_slidebot', anonymous=True)
-    
+    rospack = rospkg.RosPack()
+    rospackage_root = rospack.get_path("bark_slidebot")
+
     bark_slidebot = BarkSlidebot()
-    bark_slidebot.move_to_photo_pose()
+
+    input("Moving to photo pose, press ENTER to continue!")
+    bark_slidebot.move_to_joint_pose("photo_jpose")
+    camera_z = bark_slidebot.lookup_transform("base", "camera")[0][2]
+    print("camera_z: ", camera_z)
+
+    input("Take a photo, press ENTER to continue!")
     img = cv2.imread(os.path.join(rospackage_root, '..', '4_Color.png'))
-    bark_slidebot.bbox_prediction(img)
+
+    input("Predict bounding box, press ENTER to continue!")
+    bbox_detection_result = bark_slidebot.bbox_prediction(img)
+    bbox = bbox_detection_result.detections.detections[0].bbox
+    print("x: ", bbox.center.x, "y: ", bbox.center.y, "size_x: ", bbox.size_x, "size_y: ", bbox.size_y)
+
+    deprojection_result = bark_slidebot.deproject_2d_points_to_3d([[bbox.center.x, bbox.center.x, camera_z]])
+    bbox_centerpoint_3d = deprojection_result.points[0]
+    bbox_centerpoint_3d.z += 0.3
+    print("bbox_centerpoint_3d: ", bbox_centerpoint_3d)
+
+    rot = bark_slidebot.lookup_transform("base", "camera")[1]
+    target_pose = geometry_msgs.msg.PoseStamped()
+    target_pose.header.frame_id = "camera"
+    target_pose.pose.position = bbox_centerpoint_3d
+    target_pose.pose.orientation = geometry_msgs.msg.Quaternion(*rot)
+
+    # bark_slidebot.move_to_joint_pose("pick_jpose")
+    bark_slidebot.enable_joint_constraints()
+    bark_slidebot.move_to_pose_with_frame(target_pose)
+    bark_slidebot.disable_joint_constraints()
